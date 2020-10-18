9aa5c880e4905316dc6651b616681b9927b4ad48
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 15:52:51 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+let chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
+
+function addElementToEndOfArray(array, element) {
+  array.push(element);
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+}
+
+function accessElementInArray(array, index) {
+  return array.indexOf(index);
+}
