8c7c5953397045dc560eb6ad480606a1cd4ad9d2
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 16:18:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.splice(1);
+  array = array.splice(1);
+  return array;
 }
+
+function 
