6e3fd932f769cd8a120d48b92bb21dadec224525
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 16:10:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  let arr = [element, array...];
-  return arr;
+  array.unshift(element);
 }
